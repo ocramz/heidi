3becf08e7aff319f9ea9aa62e2cea5dd613bd687
ocramz <zocca.marco@gmail.com>
2020-10-14 19:31:44 +0200

    FIXME mkAnonProd

--- src/Data/Generics/Encode/Internal/Simple.hs
+++ src/Data/Generics/Encode/Internal/Simple.hs
@@ -39,14 +39,15 @@ data D = D (Maybe Int) (Either Int String) deriving (Eq, Show, G.Generic)
 data E = E (Maybe Int) (Maybe Char) deriving (Eq, Show, G.Generic)
 data R = R { r1 :: B, r2 :: C } deriving (Eq, Show, G.Generic, HasHeader)
 
-instance HasHeader Int where hasHeader _ = z
-instance HasHeader Char where hasHeader _ = z
-instance HasHeader () where hasHeader _ = z
+instance HasHeader Int where hasHeader _ = HPrim "Int"
+instance HasHeader Char where hasHeader _ = HPrim "Char"
+instance HasHeader () where hasHeader _ = HUnit
 instance HasHeader a => HasHeader [a]
 
 data Header =
      HProd [String] (HM.HashMap String Header) -- ^ products
-   -- | HPrim
+   | HPrim String -- ^ primitive types
+   | HUnit
    deriving (Eq, Show)
 
 instance Semigroup Header where
@@ -57,6 +58,13 @@ instance Monoid Header where
 z :: Header
 z = HProd [] mempty
 
+-- λ> hasHeader (Proxy :: Proxy R)
+-- HProd ["R"] (fromList [
+--                 ("r1",HProd ["B"] (fromList [
+--                                       ("_",HUnit)])),
+--                 ("r2",HProd ["C1","C2","C3"] (fromList [
+--                                                  ("_",HUnit)]))])
+
 class HasHeader a where
   hasHeader :: Proxy a -> Header
   default hasHeader ::
@@ -66,26 +74,28 @@ class HasHeader a where
 hasHeader' :: (All2 HasHeader xs, SListI xs) => NP ConstructorInfo xs -> Header
 hasHeader' cs = mconcat $ hcollapse $ hcliftA allp goConstructor cs 
 
-goConstructor :: All HasHeader xs => ConstructorInfo xs -> K Header xs
+goConstructor :: (All HasHeader xs) => ConstructorInfo xs -> K Header xs
 goConstructor = \case
   Record n ns -> K $ HProd [n] (mkProd ns)
-  Constructor n -> K $ HProd [n] mkAnonProd-- (mkProd)
+  Constructor n -> K $ HProd [n] mkAnonProd
+  where
+    -- labels = map (('_' :) . show) [0 ..]
 
--- hlistK :: a -> NP (K a) '[x]
+
+hlistK :: K a x -> NP (K a) '[x]
 hlistK x@(K _) = x :* Nil
 
 mkAnonProd :: HM.HashMap String Header
-mkAnonProd = HM.fromList (hcollapse $ hlistK anon)
+mkAnonProd = HM.fromList $ hcollapse $ hlistK anon
   where
-    -- labels = map (('_' :) . show) [0 ..]
-    anon :: K (String, Header) ()
+    anon :: K (String, Header) () -- FIXME should not be () but type from context
     anon = goFieldAnon "_"
 
 mkProd :: All HasHeader xs => NP FieldInfo xs -> HM.HashMap String Header
 mkProd finfo = HM.fromList $ hcollapse $ hcliftA p goField finfo
 
 goField :: forall a . (HasHeader a) => FieldInfo a -> K (String, Header) a
-goField (FieldInfo n) = goFieldAnon n -- K (n, hasHeader (Proxy :: Proxy a))
+goField (FieldInfo n) = K (n, hasHeader (Proxy :: Proxy a))
 
 goFieldAnon :: forall a . HasHeader a => String -> K (String, Header) a
 goFieldAnon n = K (n, hasHeader (Proxy :: Proxy a))
